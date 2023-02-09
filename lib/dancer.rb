b5781a24e7d6f6ce6b98c0064e94ad79969573f6
root <root@Ty-Laptop.localdomain>
2023-02-09 17:01:46 -0500

    completed assignmnet

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,25 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Dancer
+
+    include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  class Dancer
+    extend MetaDancing
+  end
+
+  class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
+
+
+end
